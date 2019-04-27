7a8a8c0543ff0f0059ce2e68a6f4e6367eb70e13
mariusamarandei <luci_12_13@yahoo.com>
2019-04-27 02:55:43 +0200

    Background Image, Logo and buttons

--- app/src/main/java/com/example/googlefirebase/MainActivity.java
+++ app/src/main/java/com/example/googlefirebase/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.googlefirebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -13,12 +15,32 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    Button buttonSignUp,buttonSignIn;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myRef.push().setValue("Hello, World MoFoooos!");
+
+        buttonSignIn = (Button) findViewById(R.id.buttonSignIn);
+        buttonSignUp = (Button) findViewById(R.id.buttonSignUp);
+
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
 
